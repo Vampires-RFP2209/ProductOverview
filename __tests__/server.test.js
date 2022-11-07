acdf37d259084a1e1899bf3544d7e119594044e8
Quyen Hoang <qduhoang@gmail.com>
2022-11-07 15:14:48 -0800

    Configured eslint to read jest and supertest modules and created 1 test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,3 +4,20 @@ const assert = require('assert')
 const express = require('express')
 
 const app = express()
+
+app.get('/products', (req, res) => {
+  res.status(200).json()
+})
+
+describe('/GET products', () => {
+  it('should receive a status 200 when making a get request to /products endpoint', () => {
+    request(app)
+      .get('/products')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err
+        console.log(res)
+      })
+  })
+})
