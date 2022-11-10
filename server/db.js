2c704d4ac784e82e717d1055165b004ff1889111
Quyen Hoang <qduhoang@gmail.com>
2022-11-10 00:23:17 -0800

    Changes to .env file to add DB_PORT

--- server/db.js
+++ server/db.js
@@ -1,22 +1,22 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const DB_PORT = 5432
-
 const db = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: DB_PORT,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
 })
+
+const PORT = process.env.DB_PORT || 5432
 
 db.connect((err) => {
   if (err) {
     console.log(err)
     return
   }
-  console.log(`Successfully connected to ${process.env.DB_DATABASE} database on port ${DB_PORT}`)
+  console.log(`Successfully connected to ${process.env.DB_DATABASE} database on port ${PORT}`)
 })
 
 // db.query(`select * from product`, (res, err) => {
