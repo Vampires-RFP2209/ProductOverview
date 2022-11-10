7f3c3a259b4d42a43f8eccdb58bd215963e15cd5
Quyen Hoang <qduhoang@gmail.com>
2022-11-10 15:22:20 -0800

    Edits to server loader.io

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
 const router = require('./routes')
 require('dotenv').config()
 
-const LOADER = path.resolve(__dirname, process.env.TEST_LOADER)
-
 // console.log(__dirname)
 const app = express()
-app.use(process.env.TEST_LOADER, express.static(LOADER))
 app.use(express.json())
 app.use(cors())
 
 app.use('/products', router)
+app.get('/loaderio-67ce1f18f770175e36b376c343a97595', (req, res) =>
+  res.send('loaderio-67ce1f18f770175e36b376c343a97595')
+)
 
 // app.get('/', (req, res) => {
 //   res.send('hello')
